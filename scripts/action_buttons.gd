1afb1b2d620848869080b59bd3d80e519ee83af4
Lucas Martins <martins7ribeiro@gmail.com>
2026-07-28 22:24:26 -0300

    feat: action buttons clicáveis

---
+++ scripts/action_buttons.gd
@@ -0,0 +1,20 @@
+extends Panel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_move_button_pressed() -> void:
+	print("move button clicado");
+	
+
+
+func _on_item_button_pressed() -> void:
+	print("item button clicado");
