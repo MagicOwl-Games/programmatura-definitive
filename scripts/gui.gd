258e03cf5c904a44d390eb745a0e51193fc07618
Lucas Martins <martins7ribeiro@gmail.com>
2026-07-30 19:34:48 -0300

    feat: items panel opening and closing

--- scripts/gui.gd
+++ scripts/gui.gd
@@ -1,8 +1,10 @@
 extends Control
 
-
+@onready var items_panel: Panel = $ItemsPanel;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	GameState.item_menu_signal.connect(_on_gui_handle_item_menu_signal);
+	items_panel.visible = false;
 	pass # Replace with function body.
 
 
@@ -10,11 +12,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _on_gui_handle_item_menu_signal(is_opening: bool) -> void:
+	items_panel.visible = is_opening;
+	$ActionButtons/ItemButton.disabled = is_opening;
+	$ActionButtons/MoveButton.disabled = is_opening;
 
 func _on_move_button_pressed() -> void:
 	print("move button clicado");
 	
-
-
 func _on_item_button_pressed() -> void:
-	print("item button clicado");
+	GameState.item_menu_signal.emit(true);
+	
+func _on_close_items_panel_button_pressed() -> void:
+	GameState.item_menu_signal.emit(false);
