eb1a69d9b1e1cfa73e26cea3c545384a4d51b19b
Lucas Martins <martins7ribeiro@gmail.com>
2026-08-30 00:49:28 -0300

    feat: prototype enemy with fixed movements - horizontal

--- readme.md
+++ readme.md
@@ -20,9 +20,24 @@ but very high number of stages.
 - [ ] Game Over Scene
 - [ ] Stage Scene
 - [ ] UI
-	- [ ] Commands (Move, Items)
+	- [ ] Commands
+		- [ ] Move player
+			- [x] The player should be able to move where there's a free tile
+			- [x] The player shouldn't be able to move where there is no free tile (wall or obstacle)
+			- [ ] "Check mate" detection. If, in the next move, there's no way to be safe, it should be game over
+		- [ ] Items
+			-	[ ] It should be possible to use Consumables
+			- [ ] It shouldn't be possible to use Collectibles
+			- [ ] Collectibles should all be lost after losing a stage and restarting
 - [ ] Detection AI (4-way detection)
+	-	[ ] The detection raycast shouldn't be pointed to a blocked tile (wall or obstacle)
 - [x] 4-way tile-based movement.
 - [ ] Turn manager
 	- [x] Enemies move after the player
 	- [ ] Smoother move transitions (affects UI directly)
+	- [ ] Queue system, with the queue visible in the UI
+		-	[ ] It should be affected by items (slow an enemy or hasting the player changes the queue)
+- [ ] Implement a turn counter
+	- [ ] There is a limited amount of turns to move
+	- [ ] The player gains bonuses if it gets to the end in less than a given amount of turns
+	- [ ] The player loses the stage if the amount of turns ends before finishing the stage
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,5 +1,20 @@
 extends CharacterBody2D
 
+var positions: Dictionary[String, Vector2] = {
+	"up": Vector2(0, -64),
+	"down": Vector2(0, 64),
+	"left": Vector2(-64, 0),
+	"right": Vector2(64, 0),
+}
+
+enum Enemy_Movement_Type {
+	HORIZONTAL,			# Move left to right, right to left
+	VERTICAL,			# Move up to down, down to up
+	L_SHAPED,			# one up/down, one left/right
+	FOUR_WAY			# one time in each direction
+};
+
+@export var movement_type: Enemy_Movement_Type;
 # TODO:
 # - each enemy should have a fixed path to follow until the player is found
 #	- i though about using areas to check the movement of the enemies, but
@@ -10,7 +25,7 @@ extends CharacterBody2D
 #more or less squares)
 # TODO:
 # - the detection area can be turned to a blocked space (walls and obstacles),
-#while it shouldn't. It's better is the enemy finds another way to face, but checking
+#while it shouldn't. The enemy should find another way to face, but checking
 #the collision shapes after repositioning the enemy is not working.
 # TODO:
 # - has_overlapping_areas collides with any area, so it is conflicting with the
@@ -21,11 +36,26 @@ enum Directions {
 	UP, DOWN, LEFT, RIGHT
 };
 
+enum Move_Direction {
+	LEFT = -1,
+	RIGHT = 1
+}
+
 @onready var up_raycast: RayCast2D = $MovementArea/UpRayCast2D;
 @onready var down_raycast: RayCast2D = $MovementArea/DownRayCast2D;
 @onready var left_raycast: RayCast2D = $MovementArea/LeftRayCast2D;
 @onready var right_raycast: RayCast2D = $MovementArea/RightRayCast2D;
-@onready var detection_area: Node2D = $DetectionArea;
+@onready var detection_area: Sprite2D = $DetectionArea;
+
+var current_enemy_x = 0;
+var current_enemy_y = 0;
+var last_enemy_x = current_enemy_x;
+var last_enemy_y = current_enemy_y;
+var next_enemy_x = 1;
+var next_enemy_y = 1;
+var move_direction = Move_Direction.RIGHT;
+
+const TILE_SIZE = 64;
 
 
 
@@ -35,6 +65,7 @@ var collision_areas: Array;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	movement_type = Enemy_Movement_Type.HORIZONTAL if movement_type == null else movement_type;
 	# FIX: WOW!!! SUCH GOOD CODE MUCH WOW
 	#collision_areas = movement_area.get_children();
 	#detection_area.hide();
@@ -48,33 +79,95 @@ func _process(delta: float) -> void:
 func _on_enemy_handle_is_moving_enemies_signal() -> void:
 	update_raycasts();
 	
-	var directions_array: Array[RayCast2D] = [
-		up_raycast,
-		down_raycast,
-		left_raycast,
-		right_raycast,
-	];
+	#var directions_array: Array[RayCast2D] = [
+		#up_raycast,
+		#down_raycast,
+		#left_raycast,
+		#right_raycast,
+	#];
 	
-	var direction_to_move: RayCast2D = null;
+	handle_enemy_movement();
+	
+	#var direction_to_move: RayCast2D = null;
+	#var direction_to_detect: RayCast2D = null;
 	
 	# FIX: this loop is breaking the game, here and down below
 	# it was breaking because at some point the directions_array was full of null
-	#values, so I had to move it to inside the function to make it work.
-	while direction_to_move == null:
-		var random_direction: Directions = Directions.values().pick_random();
-		if not directions_array[random_direction].is_colliding():
-			direction_to_move = directions_array[random_direction];
+	#values, so I had to move it inside the function to make it work.
+	#while direction_to_move == null:
+		#var random_direction: Directions = Directions.values().pick_random();
+		#if not directions_array[random_direction].is_colliding():
+			#direction_to_move = directions_array[random_direction];
+	#
+	#match direction_to_move:
+		#up_raycast:
+			#position += positions.up;
+		#down_raycast:
+			#position += positions.down;
+		#left_raycast:
+			#position += positions.left;
+		#right_raycast:
+			#position += positions.right;
+			#
+	## updating raycasts again because i'm positioning the detection area now
+	## is it necessary? is it necessary to use the raycasts at all?
+	#update_raycasts();
 	
-	match direction_to_move:
-		up_raycast:
-			position += Vector2(0, -64);
-		down_raycast:
-			position += Vector2(0, 64);
-		left_raycast:
-			position += Vector2(-64, 0);
-		right_raycast:
-			position += Vector2(64, 0);
 	
+	
+	#if direction_to_move.is_colliding():
+		#match direction_to_move:
+			#up_raycast:
+				#detection_area.position = positions.down;
+			#down_raycast:
+				#detection_area.position = positions.up;
+			#left_raycast:
+				#detection_area.position = positions.right;
+			#right_raycast:
+				#detection_area.position = positions.left;
+	#else:
+		#detection_area.position = direction_to_move.position;
+	#if direction_to_move.is_colliding():
+		#while direction_to_detect == null:
+			#var random_direction: Directions = Directions.values().pick_random();
+			#if not directions_array[random_direction].is_colliding():
+				#direction_to_detect = directions_array[random_direction];
+	
+	#match direction_to_move:
+		#up_raycast:
+			#detection_area.position = Vector2(0, -64);
+		#down_raycast:
+			#detection_area.position = Vector2(0, 64);
+		#left_raycast:
+			#detection_area.position = Vector2(-64, 0);
+		#right_raycast:
+			#detection_area.position = Vector2(64, 0);
+
+
+func handle_enemy_movement() -> void:
+	# FIX: still don't know how to handle this movement, gotta check it soon
+	
+
+	match movement_type:
+		Enemy_Movement_Type.HORIZONTAL:
+			if current_enemy_x == 1:
+				move_direction = Move_Direction.LEFT;
+			if current_enemy_x == -1:
+				move_direction = Move_Direction.RIGHT;
+
+			print("move_direction is %s" % move_direction);
+			current_enemy_x += move_direction;
+			print("current enemy x is %s" % current_enemy_x);
+
+			pass;
+		Enemy_Movement_Type.VERTICAL:
+			pass;
+		Enemy_Movement_Type.L_SHAPED:
+			pass;
+		Enemy_Movement_Type.FOUR_WAY:
+			pass;
+
+	self.position.x += (move_direction * TILE_SIZE);
 	
 # TODO: this logic should be a helper, it's duplicated
 func init_detection_area() -> void:
@@ -96,13 +189,13 @@ func init_detection_area() -> void:
 
 	match enemy_vision:
 		up_raycast:
-			detection_area.position += Vector2(0, -64);
+			detection_area.position += positions.up;
 		down_raycast:
-			detection_area.position += Vector2(0, 64);
+			detection_area.position += positions.down;
 		left_raycast:
-			detection_area.position += Vector2(-64, 0);
+			detection_area.position += positions.left;
 		right_raycast:
-			detection_area.position += Vector2(64, 0);
+			detection_area.position += positions.right;
 	
 	
 
